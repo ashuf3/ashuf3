f39a26c5eca0f099c98c82dd4bdf47a25de7d69b
Ashutosh Tiwari <atiwaricool99@gmail.com>
2024-05-26 15:45:14 +0530

    Working Header and Nav bar

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
-const inter = Inter({ subsets: ["latin"] });
+const jetbrainsMono = JetBrains_Mono({ 
+  subsets: ["latin"],
+  weight:["100","200", "300","400","500","600","700","800"],
+  variable:"--font-jetbrainsMono",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header/>
+        <PageTransition>{children}</PageTransition>
+        </body>
     </html>
   );
 }
