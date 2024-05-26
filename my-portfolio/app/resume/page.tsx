a60c5f1d816d6daa8dc4a7b04ecd31cf560721de
Ashutosh Tiwari <atiwaricool99@gmail.com>
2024-05-26 15:48:44 +0530

    Added Nav bar and Header

---
+++ my-portfolio/app/resume/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Resume() {
+  return (
+    <div>
+     Resume Page
+    </div>
+  );
+}
---
+++ my-portfolio/app/work/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Work() {
+  return (
+    <div>
+     Work Page
+    </div>
+  );
+}
